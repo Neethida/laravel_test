30039d9de1f1f715ddfd135f78409bd832f4ee7a
neethida <neethia87@gail.com>
2017-12-10 23:58:39 +0530

    nubertoword

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 require 'vendor/autoload.php';
 
 use Carbon\Carbon;
-
+use NumbertoWord\NumbertoWord;
 
 
 printf("Now: %s", Carbon::now());
+
+
+
+
+echo NumbertoWord::numberTowords(21);
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,6 +11,10 @@ class ComposerStaticInitc65e647737dfc26452c2288b6839a772
     );
 
     public static $prefixLengthsPsr4 = array (
+        'n' => 
+        array (
+            'numbertoword\\' => 13,
+        ),
         'S' => 
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
@@ -23,6 +27,10 @@ class ComposerStaticInitc65e647737dfc26452c2288b6839a772
     );
 
     public static $prefixDirsPsr4 = array (
+        'numbertoword\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
